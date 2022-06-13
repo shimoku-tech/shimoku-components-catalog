7ed71b4a60bfe1752011073e3d30aa74b2b666c6
capitan-ariete <sergi.ortiz.rodriguez@gmail.com>
2022-06-13 08:01:49 +0200

    Fixes

--- src/shimoku_components_catalog/html_components.py
+++ src/shimoku_components_catalog/html_components.py
@@ -1,11 +1,16 @@
 """HTML beautiful components"""
 
 from typing import Optional
+from re import sub
 
 
 def create_h1_title(title: str, subtitle: str) -> str:
 	return (
-		f"<div class='title'>"
+		f"<head>"
+		f"<style>.{title}{{height:100%; width:100%; border-radius:16px; padding:16px; background-color:#FAFAFB; color:#4C72F9;}}</style>"
+		f"<style>.base{{color:#202A36;}}</style>"
+		f"</head>"
+		f"<div class={title}>"
 		f"<h1>{title}</h1>"
 		f"<p class='base'>"
 		f"{subtitle}"
@@ -22,12 +27,14 @@ def beautiful_indicator(
 		background_url: str = (
 			'https://uploads-ssl.webflow.com/619f9fe98661d321dc3beec7/62a07a6d9e984908a5aca6a1_shim-anomaly-bg-s.jpg'
 		)
+
+	title_style: str = sub(r"\s+", '', title)
 	return (
 		f"<head>"
-		f"<style>.anomaly{{height:121px; width:100%; border-radius:8px; padding:45px; background-position: center; background-size: cover;background-image: url({background_url}); color:#FFFFFF;}}</style>"
+		f"<style>.{title_style}{{height:121px; width:100%; border-radius:8px; padding:45px; background-position: center; background-size: cover; background-image: url('{background_url}'); color:#FFFFFF;}}</style>"
 		f"</head>"
-		f"<a href={href}>"
-		f"<div class={title}'>"
+		f"<a href='{href}'>"
+		f"<div class='{title_style}'>"
 		f"<h3>"
 		f"{title}"
 		f"</h3>"
@@ -42,10 +49,9 @@ def button_click_to_new_tab(title: str, background_url: str, href: Optional[str]
 		href = background_url
 	return (
 		f"<head>"
-		f"<style>.pug{{height:180px; width:100%; border-radius:8px; padding:24px; background-position: center; background-size: cover; background-image: url({background_url}); color:#FFFFFF;}}</style>"
-		f"<style>.crea{{font-size:24px; color:#FFFFFF; font-weight: 400;}}</style>"
+		f"<style>.{title}{{height:180px; width:100%; border-radius:8px; padding:24px; background-position: center; background-size: cover; background-image: url('{background_url}'); color:#FFFFFF;}}</style>"
 		f"</head>"
-		f"<a href={href} target='_blank'>"
+		f"<a href='{href}' target='_blank'>"
 		f"<div class={title}>"
 		f"<p class='crea'>"
 		f"{title}"
